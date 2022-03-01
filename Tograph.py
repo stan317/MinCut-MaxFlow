fd3c03b0abb38169277084668c5415e862749a57
Veccoy <victor.bercy@imt-atlantique.net>
2022-03-01 13:53:03 +0100

    Util. functions for changing an image to the corresponding graph, according to the article "An Experimental Comparison of Min-Cut/Max-Flow Algorithms for Energy Minimization in Vision" - Victor BERCY

---
+++ Tograph.py
@@ -0,0 +1,29 @@
+import networkx as nx
+
+
+# P : original image
+# L : labeling of the original image
+# V(.,.) : interaction potential
+# set_N : set of all pairs of neighboring pixels
+
+def penalty(lp: int) -> float:
+    """
+    Compute the penalty function evaluated at the label of the image pixel
+    :param lp:
+    :return:
+    :rtype: float
+    """
+
+    pass
+
+
+def V(lp: int, lq: int) -> float:
+    """
+    Compute the interaction potential between two labels of image pixels
+    :param lp:
+    :param lq:
+    :return:
+    :rtype: float
+    """
+
+    pass
