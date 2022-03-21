fba154f27fa258aef33b6dfd511233d611a56725
Veccoy <victor.bercy@imt-atlantique.net>
2022-03-21 17:49:48 +0100

    Version finale

--- Tograph.py
+++ Tograph.py
@@ -1,16 +1,18 @@
 import sys
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+from skimage.color import rgb2gray
+from skimage.io import imread
+from tqdm import tqdm
 
 # For graph related operations
 import networkx as nx
-from skimage.color import rgb2gray
 
 # P : original image
 # L : labeling of the original image
 # V(.,.) : interaction potential
 # set_N : set of all pairs of neighboring pixels
+
 
 def get_neighbors(i, j, n, m):
     """ 
@@ -25,17 +27,17 @@ def get_neighbors(i, j, n, m):
 
     if i!=0:
         neighbors.append(str(i-1) + "," + str(j))
-    if i!=n:
+    if i!=n-1:
         neighbors.append(str(i+1) + "," + str(j))
     if j!=0:
         neighbors.append(str(i) + "," + str(j-1))
-    if j!=m:
+    if j!=m-1:
         neighbors.append(str(i) + "," + str(j+1))
 
     return neighbors
 
 
-def get_neighbors_undirect(i, j, n, m):
+def get_neighbors_undirected(i, j, n, m):
     """
     Returns only the needed neigbors to construct an undirected graph
 
@@ -46,9 +48,9 @@ def get_neighbors_undirect(i, j, n, m):
     
     neighbors = []
 
-    if i!=n:
+    if i!=n-1:
         neighbors.append(str(i+1) + "," + str(j))
-    if j!=m:
+    if j!=m-1:
         neighbors.append(str(i) + "," + str(j+1))
     
     return neighbors
@@ -70,7 +72,7 @@ def gaussian(img: np.array, mu: float, sigma: float) -> np.array:
     return num / den
 
 
-def regional_term(img: np.array, mu: np.array, sigma: np.array) -> np.array:
+def regional_term(img: np.array, mean: np.array, std: np.array) -> np.array:
     """
     Returns the log-likelihood ratio of all pixels (This is independant of X, but depends on mu and sigma)
     Corresponds to the regional term of the energy and is motivated by the MAP-MRF formulation
@@ -81,75 +83,38 @@ def regional_term(img: np.array, mu: np.array, sigma: np.array) -> np.array:
     :rtype: np.array
     """
 
-    num = gaussian(img, mu[1], sigma[1])
-    den = gaussian(img, mu[0], sigma[0])
+    gauss_obj = gaussian(img, mean[1], std[1])
+    gauss_bkg = gaussian(img, mean[0], std[0])
 
-    return np.log(num / den)
+    return np.log(gauss_obj), np.log(gauss_bkg)
 
 
 def dist(x, y):
     """
     Compute the L1-norm between two pixels of the image
 
-    :param x:
-    :param y:
+    :param x: coordinates of the first pixel
+    :param y: coordinates of the second pixel
     :rtype:   
     """
 
     return np.abs(x[0]-y[0]) + np.abs(x[1]-y[1])
 
 
-def boundary_term(img: np.array, p: list, q: list, sigma: np.array) -> float:
+def boundary_term(img: np.array, p: tuple, q: tuple, Sigma: np.array) -> float:
     """
     Compute the interaction potential between two image pixels
 
-    :param img:
-    :param p:
-    :param q:
-    :param sigma:
+    :param img: studied image
+    :param (p, q): the two nodes that are linked by the edge we want to compute the weight
+    :param Sigma: hyperparameter of the problem
     :rtype: float
     """
 
-    return np.exp(-(img[p]-img[q])**2 / (2*sigma**2)) / dist(p, q)
+    return np.exp(-(img[p[0],p[1]]-img[q[0],q[1]])**2 / (2*Sigma**2)) / dist(p, q)
 
 
-def capacity(img: np.array, p: list, q:list, mu: np.array, sigma: np.array, lambda_:float =1.) -> float:
-    """
-    Compute the capacity of an edge
-
-    :param img:
-    :param p:
-    :param q:
-    :param mu: means of the image pixels contained in the object and the background
-    :param sigma: standard deviations of the image pixels contained in the object and the background
-    :param lambda_:
-    :rtype: float
-    """
-
-    Rpq = regional_term(img, mu, sigma)
-    Bpq = boundary_term(img, p, q, sigma)
-
-    return Bpq + lambda_*Rpq
-
-
-def add_S_T(graph: nx.Graph):
-    """
-    Add the sink and source points to the graph with corresponding weights
-
-    :param
-    :rtype:
-    """
-
-    graph.add_nodes_from(["S", "T"])
-
-    K = 0
-    for x in :
-        K = np.maximum(K, np.sum(graph.get_edge_data()))
-    
-    graph.add_edge()
-
-
-def create_graph(img: np.array, mu: np.array, sigma: np.array) -> nx.Graph:
+def create_graph(img: np.array, Sigma: np.array) -> nx.Graph:
     """
     Create the directed and weighted graph associated to the given image
 
@@ -166,37 +131,134 @@ def create_graph(img: np.array, mu: np.array, sigma: np.array) -> nx.Graph:
     nodes = [str(i)+str(j) for i in range(n) for j in range(m)]
     graph.add_nodes_from(nodes)
 
-    for i in range(n):
+    for i in tqdm(range(n)):
         for j in range(m):
             
-            neighbors = get_neighbors_undirect(i, j, n, m)
+            neighbors = get_neighbors_undirected(i, j, n, m)
             
             for node in neighbors:
-
-                w = capacity(img, [i,j], [node[0],node[2]], mu, sigma)
+                v = node.split(",")
+                w = boundary_term(img, (i,j), (int(v[0]),int(v[1])), Sigma)
                 graph.add_edge(str(i)+","+str(j), node, weight=w)
-    
+
     return graph
+
+
+def add_S_T(graph: nx.Graph, img: np.array, hard_cstr:np.array, mean: np.array, std: np.array, lambda_: float =1.) -> nx.Graph:
+    """
+    Add the sink and source points to the graph with corresponding weights
+
+    :param graph : pre-constructed graph with all the nodes of the pixels linked
+    :pram
+    :rtype:
+    """
+
+    #Compute the edge value of the edges between sink or source and pixels corresponding to hard constraints
+    K = 0
+    for node in graph.nodes:
+        K = np.maximum(K, np.sum([graph.get_edge_data(node, neighbor)["weight"] for neighbor in graph.neighbors(node)]))
+    K += 1
+
+
+    #Add nodes source and sink
+    graph.add_nodes_from(["S", "T"])
+
+
+    #Add edges between sink and pixels of background hard constraint
+    bkg_cstr = np.vstack(np.where(hard_cstr[:,:,0]==219)) #red lines
+    n = bkg_cstr.shape[1]
+    bkg_cstr = bkg_cstr.reshape((n,2))
+
+    for x in tqdm(bkg_cstr):
+        graph.add_edge(str(x[0])+","+str(x[1]), "T", weight=K)
+
+
+    #Add edges between source and pixels of object hard constraint 
+    obj_cstr = np.vstack(np.where(hard_cstr[:,:,0]==255)) #white lines
+    n = obj_cstr.shape[1]
+    obj_cstr = obj_cstr.reshape((n,2))
+
+    for x in tqdm(obj_cstr):
+        graph.add_edge("S", str(x[0])+","+str(x[1]), weight=K)
+
+
+    #Add edges between the source and sink nodes and the pixels that have no hard constraint
+    no_cstr = np.vstack(np.where(hard_cstr[:,:,0]==0))
+    n = no_cstr.shape[1]
+    no_cstr = no_cstr.reshape((n,2))
+
+    RT, RS = regional_term(img, mean, std)
+
+    for x in tqdm(no_cstr):
+        graph.add_edge(str(x[0])+","+str(x[1]), "T", weight=lambda_*RT)
+        graph.add_edge("S", str(x[0])+","+str(x[1]), weight=lambda_*RS)
+
+
+    return graph
+
+
+def main(name: str, Sigma: float, lambda_: float =1.):
+    """
+    Run a set of functions to create the graph associated to the given image for minimum cut
+
+    :param name: name of the image
+    :param lambda_, Sigma : hyperparameters
+    """
+
+    img = imread("./data/images/" + name + ".jpg")
+    segmented = imread("./data/images-gt/" + name + ".png")
+    hard_cstr = imread("./data/images-labels/" + name + "-anno.png")
+
+    gray = rgb2gray(img)
+    mean = np.zeros(2)
+    std = np.zeros(2)
+
+    bkg = (segmented == 0)
+    img_bkg = gray[bkg]
+    mean[0], std[0] = img_bkg.mean(), img_bkg.std()
+
+    obj = (segmented == 255)
+    contour = (segmented == 128)
+    obj_cont = obj + contour
+    img_obj = gray[obj_cont]
+    mean[1], std[1] = img_obj.mean(), img_obj.std()
+
+    graph = create_graph(gray, Sigma)
+    graph = add_S_T(graph, gray, hard_cstr, mean, std, lambda_)
+
+    return graph
+
+
+
+#############################################################################
+
 
 
 if __name__=='main':
 
+    print('How to use the .py file from a terminal : in the folder of the file write "python ToGraph.py name lambda sigma",where name is the name of the image file, lambda and sigma are both hyperparameters')
+
     name = str(sys.argv[1])
     lambda_ = float(sys.argv[2])
+    Sigma = float(sys.argv[3])
 
-    img = cv2.open("./data/images/" + name + ".jpg")
-    segmented = cv2.open("./data/images-gt/" + name + ".png")
-    hard_cstr = cv2.open("./data/images-labels/" + name + "-anno.png")
+    img = imread("./data/images/" + name + ".jpg")
+    segmented = imread("./data/images-gt/" + name + ".png")
+    hard_cstr = imread("./data/images-labels/" + name + "-anno.png")
 
-    mu = np.zeros((1,2))
-    sigma = np.zeros((1,2))
+    gray = rgb2gray(img)
+    mean = np.zeros(2)
+    std = np.zeros(2)
 
-    B = (segmented == 0.)
-    y0 = img[B]
-    mu[0], sigma[0] = y0.mean(), y0.std()
+    bkg = (segmented == 0)
+    img_bkg = gray[bkg]
+    mean[0], std[0] = img_bkg.mean(), img_bkg.std()
 
-    O = (segmented == 1.)
-    y1 = img[O]
-    mu[1], sigma[1] = y1.mean(), y1.std()
+    obj = (segmented == 255)
+    contour = (segmented == 128)
+    obj_cont = obj + contour
+    img_obj = gray[obj_cont]
+    mean[1], std[1] = img_obj.mean(), img_obj.std()
 
-    graph = create_graph(img)
+    graph = create_graph(gray, Sigma)
+    graph = add_S_T(graph, gray, hard_cstr, mean, std, lambda_)
